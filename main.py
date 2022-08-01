27985e7b9bd2e63864b32ef830a05e8c1c5ef7b5
MorelK1 <kouhossounon.vianney@gmail.com>
2022-08-01 11:08:49 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,78 @@
+from tkinter import *
+
+window = Tk()
+window.title("Sudoku Solver")
+window.geometry("324x550")
+
+label = Label(window, text="Entrez les valeurs et appuyez sur 'Resoudre'")
+label.grid(row=0, column=1, columnspan=10)
+
+errLabel = Label(window, text="", fg="red")
+errLabel.grid(row=15, column=1, columnspan=10, pady=5)
+
+solvedLabel = Label(window, text="", fg="green")
+solvedLabel.grid(row=15, column=1, columnspan=10, pady=5)
+
+cells = {}
+
+
+def validateNumber(P):
+    out = (P.isdigit() or P == "") and len(P) < 2
+    return out
+
+
+reg = window.register(validateNumber)
+
+
+def make3x3Grid(row, column, bgcolor):
+    for i in range(3):
+        for j in range(3):
+            e = Entry(window, width=5, bg=bgcolor, justify="center", validate="key", validatecommand=(reg, "%P"))
+            e.grid(row=row + i + 1, column=column + j + 1, sticky="nsew", padx=1, pady=1, ipady=5)
+            cells[(row + i + 1, column + j + 1)] = e
+
+
+def make9x9Grid():
+    color = "#D0ffff"
+    for rowNo in range(1, 10, 3):
+        for colNo in range(0, 9, 3):
+            make3x3Grid(rowNo, colNo, color)
+            if color == "#D0ffff":
+                color = "#ffffD0"
+            else:
+                color = "#D0ffff"
+
+
+def clearValues():
+    errLabel.config(text="")
+    solvedLabel.config(text="")
+    for row in range(2, 11):
+        for col in range(1, 10):
+            cell = cells[(row, col)]
+            cell.delete(0, "end")
+
+
+def getValues():
+    board = []
+    errLabel.config(text="")
+    solvedLabel.config(text="")
+    for row in range(2, 11):
+        rows = []
+        for col in range(1, 10):
+            val = cells[(row, col)].get()
+            if val == "":
+                rows.append(0)
+            else:
+                rows.append(int(val))
+        board.append(rows)
+
+
+btn = Button(window,command=getValues,text="Resoudre",width=10)
+btn.grid(row=20,column=1,columnspan=5,pady=20)
+
+btn2 = Button(window,command=clearValues,text="Reinitialiser",width=10)
+btn2.grid(row=20,column=6,columnspan=5,pady=20)
+
+
+make9x9Grid()
+window.mainloop()
